27b138f6d7e1315a651d9f1bb6e8a129d6f8caca
MrCrankHank <mail@alexander-hank.de>
2015-07-12 00:13:47 +0200

    mvc structure

---
+++ app/views/v_menu.php
@@ -0,0 +1,22 @@
+<div class = "navbar navbar-inverse navbar-fixed-top" id="mainmenu">
+    <div class = "container">
+        <div class="navbar-header">
+            <a href = "#" class = "navbar-brand hidden-sm hidden-md">Asphaleia</a>
+
+            <button class = "navbar-toggle" data-toggle = "collapse" data-target = ".navHeaderCollapse">
+                <span class = "icon-bar"></span>
+                <span class = "icon-bar"></span>
+                <span class = "icon-bar"></span>
+            </button>
+        </div>
+        <div class = "collapse navbar-collapse navHeaderCollapse">
+            <ul class = "nav navbar-nav navbar-right">
+                <li class='hidden-sm hidden-md'><a><img hidden id="ajaxloader" src="/asphaleia/img/ajax-loader.gif"></a></li>
+
+                <li><a id="menudashboard" class="workspacetab" href='/asphaleia/dashboard'><span class="glyphicon glyphicon-dashboard"></span> <span class='hidden-sm hidden-md'>Dashboard</span></a></li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div id="workspace"></div>
