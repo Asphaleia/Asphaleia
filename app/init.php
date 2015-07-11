06eb5f2f569f824b3b653d179199b48581a7ee16
PetzJohannes <johannes.petz@hotmail.de>
2015-07-11 22:34:38 +0200

    removed files

--- app/init.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once 'core/App.php';
-require_once 'core/Controller.php';
-require_once 'core/Menu.php';
-require_once 'core/Model.php';
-?>
--- public/js/searchViews.js
+++
@@ -1,9 +0,0 @@
-$(document).on('keyup', '#searchField input[type=text]', function(){
-	searchText = $(this).val();
-	if( searchText == "") {
-  		$('#workingspace tbody tr').show();
-	} else {
-  		$('#workingspace tbody tr').hide();
-  		$('#workingspace tbody tr:contains(' + searchText + ')').show();
-	}
-});
