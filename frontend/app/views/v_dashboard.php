5991ae3c2c68328e9cc79c8b87c53d573c46f9f3
PetzJohannes <johannes.petz@hotmail.de>
2015-07-19 15:11:26 +0200

    Dashboard inital view

---
+++ frontend/app/views/v_dashboard.php
@@ -0,0 +1,24 @@
+<table id="dashboard">
+    <tr>
+        <td class="col-md-6">
+            <table id="interfaces">
+                <thead>
+                    <tr>
+                        <th class="col-md-2">Name</th>
+                        <th class="col-md-2">Interface</th>
+                        <th class="col-md-2">Up</th>
+                        <th class="col-md-4">Down</th>
+                    </tr>
+                </thead>
+                <tbody id="body">
+                    <tr>
+                        <td class="col-md-2"></td>
+                        <td class="col-md-2"></td>
+                        <td class="col-md-2"></td>
+                        <td class="col-md-2"></td>
+                    </tr>
+                </tbody>
+            </table>
+        </td>
+    </tr>
+</table>
